97f13de5f67924a7e04eacf63dbe7979d605a6ec
jkj8982@gmail.com <jkj>
2020-02-16 22:03:40 +0900

    로그인 기능 개선

--- src/main/java/com/boot/springboot/web/IndexController.java
+++ src/main/java/com/boot/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.boot.springboot.web;
 
+import com.boot.springboot.config.auth.LoginUser;
 import com.boot.springboot.config.auth.dto.SessionUser;
 import com.boot.springboot.web.dto.PostsResponseDto;
 import com.boot.springboot.service.posts.PostsService;
@@ -18,10 +19,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null) {
             model.addAttribute("userName", user.getName());
