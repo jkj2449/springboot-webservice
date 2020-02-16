0d7996550f66c79a114496ad9826e6c949df63a1
jkj8982@gmail.com <jkj>
2020-02-16 14:36:56 +0900

    Oauth2를 이용한 로그인 기능 추가

---
+++ src/main/java/com/boot/springboot/config/auth/dto/OauthAttributes.java
@@ -0,0 +1,49 @@
+package com.boot.springboot.config.auth.dto;
+
+import com.boot.springboot.domain.user.Role;
+import com.boot.springboot.domain.user.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class OauthAttributes {
+    private Map<String, Object> attributes;
+    private String nameAttributeKey;
+    private String name;
+    private String email;
+    private String picture;
+
+    @Builder
+    public OauthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
+        this.attributes = attributes;
+        this.nameAttributeKey = nameAttributeKey;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+
+    public static OauthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OauthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        return OauthAttributes.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    public User toEntity() {
+        return User.builder()
+                .name(name)
+                .email(email)
+                .picture(picture)
+                .role(Role.GUEST)
+                .build();
+    }
+}
--- src/main/java/com/boot/springboot/web/IndexController.java
+++ src/main/java/com/boot/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.boot.springboot.web;
 
+import com.boot.springboot.config.auth.dto.SessionUser;
 import com.boot.springboot.web.dto.PostsResponseDto;
 import com.boot.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
