74a834b09e15904b96fd12a9af4943b1f2f7b925
jkj8982@gmail.com <jkj>
2020-02-29 12:59:57 +0900

    프로파일 컨트롤러 생성

---
+++ src/test/java/com/boot/springboot/web/ProfileControllerSecurityTest.java
@@ -0,0 +1,33 @@
+package com.boot.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ProfileControllerSecurityTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void profile은_인증없이_호출된다() throws Exception {
+        String expected = "oauth";
+
+        ResponseEntity<String> responseEntity = testRestTemplate.getForEntity("/profile", String.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isEqualTo(expected);
+    }
+}
